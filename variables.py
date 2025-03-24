202df06facee9dc3984189942429473a38cc5962
stateside2 <ianedwardgrant@gmail.com>
2025-03-24 13:22:18 +0000

    039 Week 23/24 data update

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # ---- USED IN MENU TITLE AND WEELY WINNER FUNCTION
-week = 23
-week_thurs = 22
+week = 24
+week_thurs = 23
 
 # ---- 
 excel_file: str = "data/WINTER.xlsx"
