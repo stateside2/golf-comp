c1f5907880fa251793f945d004e85b06ac2e2b97
stateside2 <ianedwardgrant@gmail.com>
2025-08-25 15:35:58 +0100

    063 Week 19/19 data update

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # ---- USED IN MENU TITLE AND WEELY WINNER FUNCTION
-week = 18
-week_thurs = 18
+week = 19
+week_thurs = 19
 
 # ---- 
 excel_file: str = "data/SUMMER GOLF 2025.xlsx"
