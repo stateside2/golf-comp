5b085cca5e822bfd0d783e6e91f2cc6bd6db2c3a
stateside2 <ianedwardgrant@gmail.com>
2025-12-01 12:38:28 +0000

    077 Week 9/8 data update

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # ---- USED IN MENU TITLE AND WEELY WINNER FUNCTION
-week = 7
-week_thurs = 8
+week = 8
+week_thurs = 9
 
 # ---- 
 excel_file: str = "data/WINTER_GOLF_2526.xlsx"
